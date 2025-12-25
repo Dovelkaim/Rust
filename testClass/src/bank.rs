c8f27361c75ad8283c4fbd9ac9f52037629a11ac
dovelk <dovelkaim0@gmail.com>
2025-12-25 15:39:40 +0200

    see the end

--- testClass/src/bank.rs
+++ testClass/src/bank.rs
@@ -34,5 +34,64 @@ impl Bank{
         }
         u32
     }
+    pub fn transfer(&mut self, from: u32, to: u32, amount: i32) -> bool {
+        let mut tx = Transaction::new(from, to, amount);
+
+        let from_idx = self.find_account_index(from);
+        let to_idx = self.find_account_index(to);
+
+        if from_idx == -1 || to_idx == -1 {
+            self.transactions.push(tx);
+            return false;
+        }
+
+        let fi = from_idx as usize;
+        let ti = to_idx as usize;
+
+        if fi == ti {
+            // same account: just check balance
+            if self.accounts[fi].balance >= amount {
+                tx.approve();
+                self.transactions.push(tx);
+                return true;
+            } else {
+                self.transactions.push(tx);
+                return false;
+            }
+        }
+
+        // Borrow two distinct mutable references safely using split_at_mut
+        if fi < ti {
+            let (left, right) = self.accounts.split_at_mut(ti);
+            let from_acc = &mut left[fi];
+            let to_acc = &mut right[0];
+
+            if from_acc.balance >= amount {
+                from_acc.withdraw(amount);
+                to_acc.deposit(amount);
+                tx.approve();
+                self.transactions.push(tx);
+                return true;
+            } else {
+                self.transactions.push(tx);
+                return false;
+            }
+        } else {
+            let (left, right) = self.accounts.split_at_mut(fi);
+            let from_acc = &mut right[0];
+            let to_acc = &mut left[ti];
+
+            if from_acc.balance >= amount {
+                from_acc.withdraw(amount);
+                to_acc.deposit(amount);
+                tx.approve();
+                self.transactions.push(tx);
+                return true;
+            } else {
+                self.transactions.push(tx);
+                return false;
+            }
+        }
+    }
 
 }
